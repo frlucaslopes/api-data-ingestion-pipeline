7fd2c9a54adff9fce8fc74a58db3b1dadc20346f
Lucas M. Lopes <fr.lucasmartins7@gmail.com>
2026-06-09 02:23:34 -0300

    chore: remove temporary exploration file

--- explore.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-products = pd.read_json("data/raw/products.json")
-users = pd.read_json("data/raw/users.json")
-carts = pd.read_json("data/raw/carts.json")
-
-print(products.head())
-print(users.head())
-print(carts.head())
